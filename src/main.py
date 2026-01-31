089df62117dddb7959bff7987f94fe29640fe1c3
Ethan <ethanchang34@yahoo.com>
2026-01-31 12:33:32 -0800

    gesture detection

--- src/main.py
+++ src/main.py
@@ -30,7 +30,27 @@ if not cap.isOpened():
 def distance(p1, p2):
     return np.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
+def isFingerExtended(tip, pip):
+    return tip.y < pip.y
+
+def isFist(lm):
+    fingers = [
+        isFingerExtended(lm[8], lm[6]),
+        isFingerExtended(lm[12], lm[10]),
+        isFingerExtended(lm[16], lm[14]),
+        isFingerExtended(lm[20], lm[18]),
+    ]
+    return not any(fingers)
+
+def isPinch(lm, w, h, threshold=40):
+    idx = (int(lm[8].x * w), int(lm[8].y * h))
+    thumb = (int(lm[4].x * w), int(lm[4].y * h))
+    return np.linalg.norm(np.array(idx) - np.array(thumb)) < threshold
+
 frame_count = 0  # For monotonically increasing timestamp
+prev_cx = None
+GESTURE_COOLDOWN_FRAMES = 8
+cooldown = 0
 
 while True:
     ret, frame = cap.read()
@@ -70,15 +90,40 @@ while True:
             cx = int((wrist.x + mid_base.x) / 2 * w)
             cy = int((wrist.y + mid_base.y) / 2 * h)
 
-            # Gesture detection
-            if distance((idx_x, idx_y), (thumb_x, thumb_y)) < 40:
-                gesture_text = "ROTATE (Pinch)"
-            elif idx_x < cx - 20:
-                gesture_text = "LEFT"
-            elif idx_x > cx + 20:
-                gesture_text = "RIGHT"
+        # --- Gesture Detection (Intent-Based) ---
+
+        if cooldown > 0:
+            cooldown -= 1
+        else:
+            # 1. DROP (Fist)
+            if isFist(hand_landmarks):
+                gesture_text = "DROP"
+                cooldown = GESTURE_COOLDOWN_FRAMES
+
+            # 2. ROTATE (Pinch + Handedness)
+            elif isPinch(hand_landmarks, w, h):
+                handedness = hand_landmarker_result.handedness[0][0].category_name
+
+                if handedness == "Right":
+                    gesture_text = "ROTATE CW"
+                else:
+                    gesture_text = "ROTATE CCW"
+
+                cooldown = GESTURE_COOLDOWN_FRAMES
+
+            # 3. MOVE (Horizontal Motion)
             else:
-                gesture_text = "CENTER"
+                if prev_cx is not None:
+                    dx = cx - prev_cx
+
+                    if dx > 25:
+                        gesture_text = "MOVE RIGHT"
+                        cooldown = GESTURE_COOLDOWN_FRAMES
+                    elif dx < -25:
+                        gesture_text = "MOVE LEFT"
+                        cooldown = GESTURE_COOLDOWN_FRAMES
+
+                prev_cx = cx
 
     # Display gesture text
     cv2.putText(frame, gesture_text, (10, 40),
